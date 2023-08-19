2f2b241e4c7b68e67085a450dacd06ee248e7ad9
= <ahumanbeing189@gmail.com>
2023-08-18 20:04:09 -0400

    initial_commit

---
+++ LEVIATHAN/scripts/leviathan_6_brute.py
@@ -0,0 +1,30 @@
+import subprocess
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Brute force a numeric password of a binary. Takes absolute path")
+parser.add_argument('length')
+parser.add_argument('path')
+
+
+def subprocess_call(num, path):
+    p = subprocess.run(f"{path} {num} &", shell=True, capture_output=True, text=True)
+    if 'wrong' in p.stdout.lower() or 'usage' in p.stdout.lower():
+        return 0
+    return num
+    
+
+if __name__ == "__main__":  
+    args = vars(parser.parse_args())
+    if args['length'].isdecimal():
+        args['length'] = int(args['length'])
+    else:
+        raise Exception('The value length must be an integer')
+    values = list(range(10 ** args['length'] - 1)) 
+    values = map(lambda x: str(x).zfill(args['length']), values)
+    for pass_value in values:
+        attempt_result = subprocess_call(pass_value, args['path'])
+        if attempt_result:
+            print(attempt_result)
+            break
+    
